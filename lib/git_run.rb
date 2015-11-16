977148b602e632f72977ebc726f1edd1fe903a7a
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2015-11-16 19:33:09 +0100

    Add GitRun.repository

--- lib/git_run.rb
+++ lib/git_run.rb
@@ -2,10 +2,13 @@ require 'rugged'
 
 class GitRun
   def self.run(revision, command)
-    repository = Rugged::Repository.new('.')
     repository.checkout_tree(revision, strategy: :force)
     output = `#{command}`
     repository.reset('master', :hard)
     output
+  end
+
+  def self.repository
+    Rugged::Repository.new('.')
   end
 end
