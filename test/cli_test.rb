4e354f3ad9b9d8072b4ceaeda5b2dbfac9e01b8d
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2015-11-17 18:42:12 +0100

    bin/git-run returns an error message when not in a repo
    Catching `Rugged::RepositoryError` (assuming that only fires when the
    current directory is not a repository).
    
    Using `2>&1` to redirect STDERR to STDOUT in the cli_test to make it
    work with backticks (tries Minitest's assert_output, but that won't
    work).

--- test/cli_test.rb
+++ test/cli_test.rb
@@ -1,13 +1,28 @@
 require 'minitest/autorun'
 examples_path = File.expand_path(File.dirname(__FILE__) + '/../examples')
+bin_path = File.expand_path(File.dirname(__FILE__) + '/../bin/git-run')
 
 describe "git run" do
-  before do
-    Dir.chdir(examples_path)
+  describe "when not in a git repository" do
+    before do
+      @path = Dir.tmpdir
+      Dir.chdir(@path)
+    end
+
+    it "returns an error" do
+      assert_equal "/private#{@path} is not a git repository.\n",
+        `#{bin_path} master ls 2>&1`
+    end
   end
 
-  it "runs a command in a specific revision" do
-    assert_equal "This is the first revision of example.md\n",
-      `../bin/git-run 8e65d3 cat example.md`
+  describe "in a git repository" do
+    before do
+      Dir.chdir(examples_path)
+    end
+
+    it "runs a command in a specific revision" do
+      assert_equal "This is the first revision of example.md\n",
+        `#{bin_path} 8e65d3 cat example.md`
+    end
   end
 end
